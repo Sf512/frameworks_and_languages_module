8fec10da604b8650e53eea28b021230563e188d5
Sf512 <sf512@canterbury.ac.uk>
2022-01-09 23:40:59 +0000

    list items from search criteria finally completed by using if statment

--- server/app.js
+++ server/app.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const {body, oneOf, validationResult} = require ('express-validator')
 const app = express()
-app.use(express.urlencoded())
 const port = 8000
 const model = require("./dataModel.js")
 
+app.use(express.urlencoded())
+
 app.get('/item/:itemId', function (req, res) {
-    item = model.get_item(req.params["itemID"])
+    let item = model.get_item(req.params["itemID"])
     res.send(item) 
 })
 
-app.listen(port, () => {
-    console.log(`server started`)
+app.get('/items/', function (req, res) {
+    let validItems = model.searchOne(req.query ["user_id"], req.query["keywords"],req.query["lat"], req.query["lon"], req.query["radius"], req.query["date_from"], req.query["date_to"])
+    res.status(200).send(validItems)
 })
 
 app.post("/item/",body("user_id").notEmpty(),
@@ -30,9 +32,10 @@ function (req, res) {
     } else {   
         res.status(405).send("not valid")
     }
-const result = validationResult(req);
-console.log(result.array())
+    const result = validationResult(req)
+    console.log(result.array())
 })
 
-
-
+app.listen(port, () => {
+    console.log(`server started`)
+})
--- server/dataModel.js
+++ server/dataModel.js
@@ -1,8 +1,40 @@
-const items = new Map();
+const items = new Map()
 exports.get_item= function(itemId) {
-    return items.get(itemId); 
+    return items.get(itemId)
 }
 exports.add_item = function(item) {
-    itemId = Math.floor(Math.random() * 100000);
+    let itemId = Math.floor(Math.random() * 100000)
     items.set(itemId,item)
+}
+exports.searchOne = function(user_id, keywords, lat, lon, radius, date_from, date_to) {
+    let result = []
+    items.forEach (function(item){
+        let isValid= true
+
+        if(item["user_id"] !=user_id && user_id != undefined) {
+            isValid= false
+        }
+        if(item["keywords"] !=keywords && keywords != undefined) {
+            isValid= false
+        }
+        if(item["lat"] !=lat && lat != undefined) {
+            isValid= false
+        }
+        if(item["lon"] !=lon && lon != undefined) {
+            isValid= false
+        }
+        if(item["radius"] !=radius && radius != undefined) {
+            isValid= false
+        }
+        if(item["date_from"] !=date_from && date_from != undefined) {
+            isValid= false
+        }
+        if(item["date_to"] !=date_to && date_to != undefined) {
+            isValid= false
+        }
+        if (isValid) {
+            result.push(item)
+        }
+    })
+    return result
 }
